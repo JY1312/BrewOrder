5e58fa51b6dbbd233ae94f3f1d25183768253ddc
HYX20011209 <spacetime57@163.com>
2024-09-05 23:55:15 -0400

    format response

--- src/main/java/com/beerpub/order/controller/UserController.java
+++ src/main/java/com/beerpub/order/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.beerpub.order.controller;
 
+import com.beerpub.order.Response;
 import com.beerpub.order.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UserController {
@@ -12,21 +14,21 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@RequestParam String username, @RequestParam String password,
+    public Response signup(@RequestParam String username, @RequestParam String password,
                          @RequestParam String phoneNumber, @RequestParam String role){
         if (userService.signup(username, password, phoneNumber, role)){
-            return "success";
+            return Response.newSuccess("user " + username + " sign-up successfully");
         }else{
-            return "fail";
+            return Response.newFail("user " + username + " already exists");
         }
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password){
+    public Response login(@RequestParam String username, @RequestParam String password){
         if (userService.login(username, password)){
-            return "success";
+            return Response.newSuccess("user " + username + " login successfully");
         }else{
-            return "fail";
+            return Response.newFail("login failed");
         }
     }
 }
